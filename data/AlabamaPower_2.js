e281687329ef242e9bbb62b44bed180201594a14
Giocos5 <107521986+Giocos5@users.noreply.github.com>
2026-05-10 23:02:40 -0400

    Delete data directory

--- data/AlabamaPower_2.js
+++
@@ -1,1 +0,0 @@
-var json_AlabamaPower_2 = {"type":"FeatureCollection","name":"AlabamaPower_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"xy_coordinate_resolution":1e-06,"features":[]}
